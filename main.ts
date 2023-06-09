f6736edb715360a0c7a8e664cd097c879d3bff68
noa-micro <121046810+noa-micro@users.noreply.github.com>
2023-06-09 20:16:26 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,12 @@
 input.onButtonPressed(Button.A, function () {
-    wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 61)
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 110)
+    basic.pause(5000)
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
 })
 input.onButtonPressed(Button.B, function () {
 	
 })
+wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
 basic.forever(function () {
 	
 })
