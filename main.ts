5b8db48c1e3685d8a7452f18f9491a9930c1d58b
noa-micro <121046810+noa-micro@users.noreply.github.com>
2023-06-09 19:23:55 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 61)
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+	
+})
